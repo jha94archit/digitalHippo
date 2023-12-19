59895c9446a3a9f07471f92e2e33ac367b9c39e6
Archit Jha <architjha@Archits-MacBook-Pro.local>
2023-12-19 11:27:37 +0530

    Minimal Navbar and Landing Page

---
+++ src/config/index.tsx
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    feature: [
+      {
+        name: "Editor's picks",
+        href: "#",
+        imgSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imgSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imgSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    feature: [
+      {
+        name: "Favourite Icon Picks",
+        href: "#",
+        imgSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imgSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imgSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
