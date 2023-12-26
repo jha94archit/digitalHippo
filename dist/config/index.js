9ba7a508f03031029c1c4e3ebfa930e37d56053d
Archit Jha <architjha@Archits-MacBook-Pro.local>
2023-12-26 23:28:18 +0530

    CompleteApp Version 1

---
+++ dist/config/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRODUCT_CATEGORIES = void 0;
+exports.PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits",
+        featured: [
+            {
+                name: "Editor's picks",
+                href: "#",
+                imgSrc: "/nav/ui-kits/mixed.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imgSrc: "/nav/ui-kits/blue.jpg",
+            },
+            {
+                name: "Bestsellers",
+                href: "#",
+                imgSrc: "/nav/ui-kits/purple.jpg",
+            },
+        ],
+    },
+    {
+        label: "Icons",
+        value: "icons",
+        featured: [
+            {
+                name: "Favourite Icon Picks",
+                href: "#",
+                imgSrc: "/nav/icons/picks.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imgSrc: "/nav/icons/new.jpg",
+            },
+            {
+                name: "Bestsellers",
+                href: "#",
+                imgSrc: "/nav/icons/bestsellers.jpg",
+            },
+        ],
+    },
+];
